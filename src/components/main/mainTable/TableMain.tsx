40304cbff4b14a2d87c9d496670a40280c9af36e
Maksim Tananykin <maks.tananykin.20177@mail.ru>
2022-09-08 14:46:37 +0300

    add Skeleton to main table

--- src/components/main/mainTable/TableMain.tsx
+++ src/components/main/mainTable/TableMain.tsx
@@ -1,4 +1,6 @@
 import "../main.scss";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 import arrowUp from "./mainTableIcons/arrowUp.svg";
 import arrowDown from "./mainTableIcons/arrowDown.svg";
@@ -10,9 +12,10 @@ import Button from "../../generic/button/Button";
 
 export interface TableProps {
   dataToShow: Array<CryptFromFetch>;
+  pageWidth: number;
 }
 
-export default function TableMain({ dataToShow }: TableProps) {
+export default function TableMain({ dataToShow, pageWidth }: TableProps) {
   const navigate = useNavigate();
 
   const [isPopupOpen, setIsPopupOpen] = useState<boolean>(false);
@@ -39,43 +42,72 @@ export default function TableMain({ dataToShow }: TableProps) {
         <p className="change crypts-header__item">Change (24h)</p>
         <p className="add crypts-header__item">Add to Wallet</p>
       </div>
-      {dataToShow.map((crypt: CryptFromFetch) => {
-        return (
-          <div
-            className="crypts__line"
-            key={crypt.id}
-            onClick={() => aboutCrypt(crypt.id)}
-          >
-            <p className="rank">{crypt.rank}</p>
-            <p className="name adaptive">{crypt.name}</p>
-            <p className="symbol">{crypt.symbol}</p>
-            <p className="price">{(+crypt.priceUsd).toFixed(5)}$</p>
-            <div className="change">
-              <div className="change__content">
-                <p
-                  className={
-                    crypt.changePercent24Hr < 0 ? "change__content_low" : ""
-                  }
+      {dataToShow.length
+        ? dataToShow.map((crypt: CryptFromFetch) => {
+            return (
+              <div
+                className="crypts__line"
+                key={crypt.id}
+                onClick={() => aboutCrypt(crypt.id)}
+              >
+                <p className="rank">{crypt.rank}</p>
+                <p className="name adaptive">{crypt.name}</p>
+                <p className="symbol">{crypt.symbol}</p>
+                <p className="price">{(+crypt.priceUsd).toFixed(5)}$</p>
+                <div className="change">
+                  <div className="change__content">
+                    <p
+                      className={
+                        crypt.changePercent24Hr < 0 ? "change__content_low" : ""
+                      }
+                    >
+                      {crypt.changePercent24Hr > 0 && "+"}
+                      {(+crypt.changePercent24Hr).toFixed(4)}%
+                    </p>
+                    <img
+                      src={crypt.changePercent24Hr < 0 ? arrowDown : arrowUp}
+                      alt="arrow"
+                      className="change__image adaptive"
+                    />
+                  </div>
+                </div>
+                <Button
+                  onClick={(e) => addCryptToWallet(e, crypt)}
+                  buttonType="button_action"
                 >
-                  {crypt.changePercent24Hr > 0 && "+"}
-                  {(+crypt.changePercent24Hr).toFixed(4)}%
-                </p>
-                <img
-                  src={crypt.changePercent24Hr < 0 ? arrowDown : arrowUp}
-                  alt="arrow"
-                  className="change__image adaptive"
-                />
+                  Add
+                </Button>
               </div>
+            );
+          })
+        : [...Array(10).keys()].map((line) => (
+            <div className="crypts__line" key={line}>
+              <Skeleton className="skeleton" width={20} height={20} />
+              {pageWidth > 640 && (
+                <Skeleton className="skeleton" width={170} height={20} />
+              )}
+              <Skeleton
+                className="skeleton"
+                width={pageWidth > 640 ? 50 : 30}
+                height={20}
+              />
+              <Skeleton
+                className="skeleton"
+                width={pageWidth > 640 ? 150 : 100}
+                height={20}
+              />
+              <Skeleton
+                className="skeleton"
+                width={pageWidth > 640 ? 110 : 60}
+                height={20}
+              />
+              <Skeleton
+                className="skeleton"
+                width={pageWidth > 640 ? 72 : 52}
+                height={pageWidth > 640 ? 44 : 34}
+              />
             </div>
-            <Button
-              onClick={(e) => addCryptToWallet(e, crypt)}
-              buttonType="button_action"
-            >
-              Add
-            </Button>
-          </div>
-        );
-      })}
+          ))}
       <AddCryptModal
         isPopupOpen={isPopupOpen}
         setIsPopupOpen={setIsPopupOpen}
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react";
 import "../components/main/main.scss";
 
-import spinner from "../components/generic/icons/spinner.svg";
 import TableMain from "../components/main/mainTable/TableMain";
 import Button from "../components/generic/button/Button";
 
 import { useQuery } from "@apollo/client";
 import { GET_ALL_CRYPTS, GET_PAGES_AMOUNT } from "../lib/query/crypt";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 export default function MainPage() {
+  const pageWidth = document.documentElement.scrollWidth;
+
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [offset, setOffset] = useState<number>(0);
 
@@ -47,36 +50,46 @@ export default function MainPage() {
   return (
     <main className="content main">
       <h2 className="title">All Cryptocurrency</h2>
-      {loading || pagesLoading ? (
-        <img className="preloader__item" src={spinner} alt="spinner" />
-      ) : (
-        <div>
-          <TableMain dataToShow={allCrypts.getAllCrypts} />
+      <TableMain
+        dataToShow={allCrypts?.getAllCrypts || []}
+        pageWidth={pageWidth}
+      />
 
-          <section className="pagination">
-            <Button size="size_sm" buttonType="button_slide" onClick={prevPage}>
-              Prev
-            </Button>
-            {[...Array(Math.ceil(pagesAmount.getPagesAmount / 10)).keys()].map(
-              (page: number) => {
-                return (
-                  <Button
-                    key={page}
-                    size="size_sm"
-                    buttonType="button_pagination"
-                    onClick={(e) => changePage(e)}
-                    active={page + 1 === currentPage}
-                  >
-                    {page + 1}
-                  </Button>
-                );
-              }
-            )}
-            <div className="current__page">{currentPage}</div>
-            <Button size="size_sm" buttonType="button_slide" onClick={nextPage}>
-              Next
-            </Button>
-          </section>
+      {!pagesLoading ? (
+        <section className="pagination">
+          <Button size="size_sm" buttonType="button_slide" onClick={prevPage}>
+            Prev
+          </Button>
+          {[...Array(Math.ceil(pagesAmount.getPagesAmount / 10)).keys()].map(
+            (page: number) => {
+              return (
+                <Button
+                  key={page}
+                  size="size_sm"
+                  buttonType="button_pagination"
+                  onClick={(e) => changePage(e)}
+                  active={page + 1 === currentPage}
+                >
+                  {page + 1}
+                </Button>
+              );
+            }
+          )}
+          <div className="current__page">{currentPage}</div>
+          <Button size="size_sm" buttonType="button_slide" onClick={nextPage}>
+            Next
+          </Button>
+        </section>
+      ) : (
+        <div className="pagination">
+          {[...Array(pageWidth > 640 ? 12 : 3).keys()].map((page: number) => (
+            <Skeleton
+              key={page}
+              className="skeleton-pagination"
+              width={40}
+              height={40}
+            />
+          ))}
         </div>
       )}
     </main>
